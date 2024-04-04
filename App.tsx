0973cf30b70e71c975bf30e177c524a0004f90ef
Ramonprog <programador.ramon@gmail.com>
2024-04-04 08:25:18 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventeDate}>Sexta, 4 de Novembro de 2020</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#131016',
+    padding: 24,
   },
+  eventName: {
+    color: '#fdfcfe',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  eventeDate: {
+    color: '#6b6b6b',
+    fontSize: 16,
+  }
 });
